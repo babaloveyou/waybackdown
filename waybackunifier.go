301592050169c3ba6e66effcd3a66ecd66283923
babaloveyou <babaloveyou@users.noreply.github.com>
2021-02-08 13:24:33 +0800

    Update waybackunifier.go

--- waybackunifier.go
+++ waybackunifier.go
@@ -44,6 +44,7 @@ func main() {
 			defer func() { <-lim }()
 			//unifySnapshots(snapshot, subs)
 			fmt.Println("https://web.archive.org/web/",snapshot[0],"if_/",snapshot[1])
+			fmt.Println("1111111111%s/%s",snapshot[0],snapshot[1])
 			if err != nil {
 				log.Printf("couldn't unify snapshots: %v", err)
 			}
